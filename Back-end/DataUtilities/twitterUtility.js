5ebe6761b7eca7dff90fe697e2101b57dbd58c07
muneeb-121 <37483107+muneeb-121@users.noreply.github.com>
2019-10-09 13:38:58 +0500

    updated

--- Back-end/DataUtilities/twitterUtility.js
+++ Back-end/DataUtilities/twitterUtility.js
@@ -6,8 +6,8 @@ module.exports.getTweets = async(queryText,count) => {return new Promise( (resol
         let oauth = new oAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
             'https://api.twitter.com/oauth/access_token',
-            'xn3vRsyNkiafM2BEsUOfR376j',
-            'WKwV1dXteUTUQLDuHeEsiT6L0QYEKcD8TGn2s1U6KNs5pJNjiN',
+            'consumer API key',
+            'consumer API key secret',
             '1.0A',
             null,
             'HMAC-SHA1'
@@ -15,8 +15,8 @@ module.exports.getTweets = async(queryText,count) => {return new Promise( (resol
 
         oauth.get(
             `https://api.twitter.com/1.1/search/tweets.json?q=${queryText}&count=${count}&include_entities=true&with_twitter_user_id=true&result_type=mixed`,
-            '487175114-beRb3vg8n8Q3w91KeSY31IGnDi54qmD3yXGhOeM0', //test user token
-            'CYVWbhIAxmDkgOUu2y2KsvcRMamJ5Gg9BnDnj9kX30VS4', //test user secret            
+            'You access token', //test user token
+            'Your token-secret', //test user secret            
             function (error, data){
               if (error) resolve({success:false,data:error});        
               resolve({success:true,data:JSON.parse(data)});
